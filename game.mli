a889687e9bc0e6c4d43d3ee31fd0aa2d0fcb1ad6
Jesse Lupica <jrl344@cornell.edu>
2015-11-08 17:51:11 -0500

    added apply_status_debuffs

--- game.mli
+++ game.mli
@@ -47,6 +47,11 @@ val get_player_action : player_state -> public_info -> action
 val fight : game_state -> action -> action -> game_state
 
 (*
+* After both players have gone, this function applies status debuffs such as burn 
+*)
+val apply_status_debuffs : game_state -> game_state 
+
+(*
 * The recursive driver for the game - takes in the initial game state at first,
 * gets the actions from each player, calls fight, and passes the new game state
 * returned by fight as a recursive call
