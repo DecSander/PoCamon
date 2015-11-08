223cf57e2b072301c46dc71fb8f4014235531ebd
Jesse Lupica <jrl344@cornell.edu>
2015-11-08 16:49:09 -0500

    changed type of Switch

--- game.mli
+++ game.mli
@@ -2,7 +2,7 @@
 * An action that the player can take as his/her turn -
 * Use a pocamon's move, or switch pocamon
 *)
-type action = Move of move | Switch of pocamon
+type action = Move of move | Switch of pocamon * pocamon
 
 (*
 * Current state information about a player
