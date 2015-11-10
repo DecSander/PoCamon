04de1aedd23da5f98d70f956d858611297b1dc5a
DecSander <dms497@cornell.edu>
2015-11-09 20:55:14 -0500

    moved input.mli to io.mli

--- io.mli
+++ io.mli
@@ -1,7 +1,17 @@
 open Pocamon
 
+type command = Up | Down | Enter | Action of action | Fight | Bag | Pocamon
+| Run | Back
+
+(*
+ * Takes a string and parses it using regex to return the command corresponding
+ * to that string. If the parser is unable to determind the command, None is
+ * returned
+ *)
+val process_input : string -> command option
+
 (* Prints the appropriate screen for the given command specified *)
-val print_screen : game_state -> command -> unit 
+val print_screen : game_state -> command -> unit
 
 val create_pocamon_ascii : pocamon -> bytes
 
