6610410d18bf7ea7585754b6ec6168f39a86c280
DecSander <dms497@cornell.edu>
2015-11-20 20:08:55 -0500

    started on io

---
+++ io.ml
@@ -0,0 +1,94 @@
+type command = Up | Down | Enter | Action of action | Fight | Bag | Pocamon
+| Run | Back | Save | Load
+
+let match_phrase (str: string) (regex: string) :bool =
+  Str.string_match (Str.regexp regex) str 0
+
+let get_switch (str: string) :command option =
+  let words = Str.split " " str in
+  if List.length = 3
+  then Some (Action (Switch ((List.nth 1), (List.nth 2))))
+  else if List.length = 4
+  then Some (Action (Switch ((List.nth 2), (List.nth 4))))
+else None
+
+
+let process_input (s: string) :command option =
+  let trim_s = String.uppercase (String.trim s) in
+  if match_phrase trim_s "^UP$" then Some Up
+  else if match_phrase trim_s "^DOWN$" then Some Down
+  else if match_phrase trim_s "^$" then Some Enter
+  else if match_phrase trim_s "^FIGHT$" then Some Fight
+  else if match_phrase trim_s "^BAG$" then Some Bag
+  else if match_phrase trim_s "^POCAMON$" then Some Pocamon
+  else if match_phrase trim_s "^RUN$" then Some Run
+  else if match_phrase trim_s "^BACK$" then Some Back
+  else if match_phrase trim_s "^SAVE$" then Some Save
+  else if match_phrase trim_s "^LOAD$" then Some Load
+  else if match_phrase trim_s "^SWITCH" then get_switch trim_s
+  else Some (Action of (Move of trim_s))
+
+let create_pocamon_ascii (pc: pocamon) :bytes =
+  pocamon.ascii
+
+let create_health_bar (pc: pocamon) :bytes =
+  let rec gen_hp_bar (equals: int) (empty: int) (s: bytes) :bytes =
+    if empty = 0 then s ^ "]"
+    else if equals = 0 then gen_hp_bar 0 (empty - 1) (s ^ " ")
+    else gen_hp_bar (equals - 1) empty (s ^ "=") in
+
+  let status_to_string (status: pStatus) :bytes =
+  match stat with
+  | SNormal -> "   "
+  | SPoison -> "PSN"
+  | SBurn -> "BRN"
+  | SSleep -> "SLP"
+  | SParalyze -> "PAR"
+  | SFreeze -> "FRZ" in
+
+  let frac = (float_of_int pc.health) /. (float_of_int pc.stats.max_HP) in
+  let max_hp = 22 in
+  let equals = (int_of_float (frac *. (float_of_int max_hp)))
+  (gen_HP_bar equals (max_hp - equals) "[")
+  ^ " " ^ (string_of_int pc.health) ^ "/" ^ (string_of_int pc.stats.max_HP)
+  ^ " " ^ status_to_string (pc.status) ^ " "
+
+
+
+let art_joiner (art1: bytes) (art2: bytes) :bytes =
+  let rec art_help (art1: bytes list) (art2: bytes list) (result: bytes) :bytes =
+    match art1, art2 with
+    | h1::t1, h2::t2 -> art_help t1 t2 (result ^ "\n" ^ h1 ^ "    ||    " ^ h2)
+    | _, _ -> result in
+
+  art_help (Str.split (Str.regexp "\n") art1)
+           (Str.split (Str.regexp "\n") art2)
+           ""
+
+let create_help (c_list: command list) :bytes =
+  let command_to_string (cmd: command) :bytes =
+    match cmd with
+    | Up -> "Up"
+    | Down -> "Down"
+    | Enter -> "Enter"
+    | Fight -> "Fight"
+    | Bag -> "Bag"
+    | Pocamon -> "Pocamon"
+    | Run -> "Run"
+    | Back -> "Back"
+    | Save -> "Save"
+    | Load -> "Load"
+    | Action -> "<Move> \n Switch <Pocamon> with <Pocamon>" in
+
+  let rec help_help (cl: command list) (result: bytes) :bytes =
+    match cl with with
+    | [] -> result
+    | h::t -> help_help t (result ^ "\n" ^ (command_to_string h)) in
+
+  help_help c_list ""
+
+let create_available_pocamon (p_list: pocamon_list) (i: int) :bytes =
+  let start = if List.length p_list <= 4 then 0 else i in
+  let ellipses = (List.length p_list > 4) && (i = ((List.length p_list) - 4)) in
+  let end = if List.length p_list <= 4 then List.length else
+    if ellipses then i + 4 else i + 3
--- io.mli
+++ io.mli
@@ -3,6 +3,9 @@ open Types
 type command = Up | Down | Enter | Action of action | Fight | Bag | Pocamon
 | Run | Back | Save | Load
 
+type screen_state = Out | Moves of int | Pocamon of int | Bag |
+Result (string * bool * string * pEffect)
+
 (*
  * Takes a string and parses it using regex to return the command corresponding
  * to that string. If the parser is unable to determind the command, None is
@@ -11,7 +14,7 @@ type command = Up | Down | Enter | Action of action | Fight | Bag | Pocamon
 val process_input : string -> command option
 
 (* Prints the appropriate screen for the given command specified *)
-val print_screen : game_state -> command -> unit
+val print_screen : game_state -> screen_state -> unit
 
 (* The below functions will be used in creating print_screen, but will
  * be removed from the mli upon implementation because they should
@@ -25,8 +28,6 @@ val create_health_bar : pocamon -> bytes
 val create_available_pocamon : pocamon list -> bytes
 
 val create_available_moves : pocamon -> bytes
-
-val create_battle : battle_status -> bytes
 
 val create_help : command list -> bytes
 
