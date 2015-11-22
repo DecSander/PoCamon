e56218b1a897c269cad482bda5346e4d244233d4
DecSander <dms497@cornell.edu>
2015-11-22 11:14:41 -0500

    Finished testing input processing

--- test_io.ml
+++ test_io.ml
@@ -1,23 +1,9 @@
 open Io
-
-let command_to_string co =
-  match co with
-  | None -> "None\n"
-  | Some v -> (match v with
-    | Up -> "Up"
-    | Down -> "Down"
-    | Enter -> "Enter"
-    | Action _ -> "Action"
-    | Fight -> "Fight"
-    | Pocamon -> "Pocamon"
-    | Run -> "Run"
-    | Back -> "Back"
-    | Save -> "Save"
-    | Load -> "Load");;
+open Types
 
 TEST "Up" = process_input "up" = Some Up
 TEST "Down" = process_input "down" = Some Down
-TEST "Trimming" = process_input " down \t" = Some Down
+TEST "Trim" = process_input " down \t" = Some Down
 TEST "Uppercase" = process_input "DOWN" = Some Down
 TEST "Lowercase" = process_input "down" = Some Down
 TEST "Mixed Case" = process_input "dOwN" = Some Down
@@ -26,3 +12,9 @@ TEST "Fight" = process_input "fight" = Some Fight
 TEST "Pocamon" = process_input "pocamon" = Some Pocamon
 TEST "Run" = process_input "run" = Some Run
 TEST "Back" = process_input "back" = Some Back
+TEST "Save" = process_input "save" = Some Save
+TEST "Load" = process_input "load" = Some Load
+TEST "Switch" = process_input "switch raichu" = Some (Action (Switch "RAICHU"))
+TEST "Bad Switch" = process_input "switch james jfsdf" = None
+TEST "Move" = process_input "win" = Some (Action (Move "WIN"))
+TEST "2-word move" = process_input "two move" = Some (Action (Move "TWO MOVE"))
