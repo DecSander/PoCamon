ab5ec9cacf09f434d8c6cb2cacaca21d8fa31643
DecSander <dms497@cornell.edu>
2015-11-22 11:16:33 -0500

    fixed AI typos

--- ai.ml
+++ ai.ml
@@ -3,14 +3,14 @@ type ai_player = P1 | P2
 
 let status_score (s: pStatus) :float =
   match s with
-  | SNormal -> 0
+  | SNormal -> 0.
   | SPoison -> 0.2
   | SBurn -> 0.3
   | SSleep n -> (float_of_int n) *. 0.2
   | SParalyze -> 0.4
   | SFreeze n -> (float_of_int n) *. 0.2
 
-let health_score (p_list: pocamon list) (score: float) :float =
+let rec health_score (p_list: pocamon list) (score: float) :float =
   match p_list with
   | [] -> score
   | h::t -> health_score t
@@ -19,8 +19,8 @@ let health_score (p_list: pocamon list) (score: float) :float =
 
 let player_score (ps: player_state) :float =
   let status = status_score ps.active_pocamon.status in
-  let health = health_score ps.pocamon_list 0 in
-  health_score -. status_score
+  let health = health_score ps.pocamon_list 0. in
+  health -. status
 
 let game_score (ai: ai_player) (gs: game_state) :float =
   match ai with
