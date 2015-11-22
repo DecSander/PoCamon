7f091996bcd39695b34b7f0c0c00ae98d4b748d0
DecSander <dms497@cornell.edu>
2015-11-22 16:54:11 -0500

    added start-screen tests to test_io.ml

--- test_io.ml
+++ test_io.ml
@@ -18,3 +18,12 @@ TEST "Switch" = process_input "switch raichu" = Some (Action (Switch "RAICHU"))
 TEST "Bad Switch" = process_input "switch james jfsdf" = None
 TEST "Move" = process_input "win" = Some (Action (Move "WIN"))
 TEST "2-word move" = process_input "two move" = Some (Action (Move "TWO MOVE"))
+
+TEST "Yes" = process_selection "Yes" = Some Yes
+TEST "No" = process_selection "No" = Some No
+TEST "y" = process_selection "y" = Some Yes
+TEST "n" = process_selection "n" = Some No
+TEST "Trim" = process_selection "  \t yes \t  " = Some Yes
+TEST "Bad input" = process_selection "sfdsd" = None
+TEST "No at end" = process_selection "jamno" = None
+TEST "Yes in middle" = process_selection "jkyeskl" = None
