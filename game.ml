e751dd2e253e2799b5cba19fb0d4a4d319df10b2
jamessto <jms852@cornell.edu>
2015-11-24 13:48:07 -0500

    finished status change moves

--- game.ml
+++ game.ml
@@ -241,7 +241,7 @@ let print_result action g_state p_state m_status opp_p_state : unit =
             (Talking change_string) else ()) in
 
         match a.spec_eff with
-        | MNone | MExplode -> ()
+        | MNone | MExplode | MCharge -> ()
         | MRecover | MRecoil | MAttack _ | MDefense _ | MSpecAttack _
         | MSpecDefense _ | MSpeed _ | MAllStatsUp ->
           wait_for_enter g_state p_state (Talking (
