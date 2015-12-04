5913c4180c880852510da835753aa798226df906
DecSander <dms497@cornell.edu>
2015-12-03 19:08:45 -0500

    changed win texts

--- game.ml
+++ game.ml
@@ -14,42 +14,42 @@ let trainers =
   [{start_text="Prof. White: Welcome to the Elite 7, now prepare to lose";
   name="Prof. White";
   end_text="Prof. White: The next game design project is gonna be to beat you";
-  win_text="CS might not be right for you. Maybe try OR";
+  win_text="Prof. White: CS might not be right for you. Maybe try OR";
   pocamon_list=["HORSEA"; "METAPOD";
   "PIDGEOT"; "SPEAROW"; "POLIWAG"; "MEOWTH"]};
 
   {start_text="Prof. Bracy: Prepare to have your cache cleared";
   name="Prof. Bracy";
   end_text="Prof. Bracy: You optimized the common case faster than I could :(";
-  win_text="Am I surprised? Not even a BIT";
+  win_text="Prof. Bracy: Am I surprised? Not even a BIT";
   pocamon_list=["ODDISH"; "BELLSPROUT";
   "PARAS"; "CUBONE"; "BEEDRILL"; "VENOMOTH"]};
 
   {start_text="Prof. Gries: You're gonna need more than just java after this";
   name="Prof. Gries";
   end_text="Prof. Gries: I guess I need more loop invariants";
-  win_text="Don't worry. There's always the optional final";
+  win_text="Prof. Gries: Don't worry. There's always the optional final";
   pocamon_list=["EEVEE"; "VILEPLUME";
   "POLIWRATH"; "HAUNTER"; "MR.MIME"; "PSYDUCK"]};
 
   {start_text="Team Rocket: Prepare for Trouble ... and make it Double";
   name="Team Rocket";
   end_text="Team Rocket: Oh this is PoCamon? We thought it was Pokemon";
-  win_text="";
+  win_text="Team Rocket: Team Rocket blasts off again!";
   pocamon_list=["EKANS"; "VAPOREON";
   "KOFFING"; "MEOWTH"; "PIKACHU"; "GASTLY"]};
 
   {start_text="Prof. Hopcroft: I won my Turing Award for beating kids like you";
   name="Prof. Hopcroft";
   end_text="Prof. Hopcroft: If only I had more 2-3 Trees";
-  win_text="I didn't win the Turing Award for nothing";
+  win_text="Prof. Hopcroft: I didn't win the Turing Award for nothing";
   pocamon_list=["ARCANINE"; "CHANSEY";
   "MACHAMP"; "ONIX"; "AERODACTYL"; "GYARADOS"]};
 
   {start_text="Prof. George: My NFA beats your DFA anyday";
   name="Prof. George";
   end_text="Prof. George: I guess optimzing pocamon is an NP hard problem...";
-  win_text="";
+  win_text="Prof. George: I think your proof is backwards";
   pocamon_list=["DRAGONAIR"; "CHARIZARD";
   "RHYDON"; "MEW"; "SNORLAX"; "BLASTOISE"]};
 
@@ -57,7 +57,7 @@ let trainers =
   name="Prof. Clarkson";
   end_text=
     "Prof. Clarkson: Holy Zardoz! I ca-ca-can't believe I lost. Congrats!!!";
-  win_text="";
+  win_text="Prof. Clarkson: You must have used imperative features!";
   pocamon_list=["DRAGONITE"; "ALAKAZAM";
   "GENGAR"; "VENUSAUR"; "MEWTWO"; "LAPRAS"]}]
 
